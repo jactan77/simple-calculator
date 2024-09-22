227836fd66cdd01134355be5a2b7ba06a53f3d0a
jtdev632 <jaccek.bid@gmail.com>
2024-09-23 00:29:08 +0200

    change

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI: "mongodb+srv://jacek:markap@cluster0.b1x4d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    MongoURI: "mongodb+srv://<username>:<password>@cluster0.mongodb.net/<dbname>?retryWrites=true&w=majority"
 }
